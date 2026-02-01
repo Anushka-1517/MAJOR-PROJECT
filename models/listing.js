659fb60e7f861703ffc8199576ced08a2b41ba5e
Anushka Sangal <143788228+Anushka-1517@users.noreply.github.com>
2026-02-01 21:46:17 +0530

    Add files via upload

---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose =require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:  {
+        type: String,
+        required: true,
+    },
+    description:String,
+    image:{
+
+        filename:{
+            type:String,
+            default:"listingimage",
+        },
+        url:{
+            type:String,
+            default:"https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+        },
+    },
+    price:Number,
+    location:String,
+    country:String,
+});
+const Listing = mongoose.model("Listing",listingSchema );
+module.exports = Listing;
+  
