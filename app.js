fcba242b4b7094b9c94d6f4658ac142434610933
Anushka-1517 <anushkasangal15@gmail.com>
2026-06-01 02:43:09 +0530

    Add review routes and classroom Express routing practice

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
 
-const Listing = require("./models/listing.js");
-const wrapAsync = require("./utils/wrapAsync.js");
+
+
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
+
+
+const listings=require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+
 
 // ======================
 // MongoDB Connection
@@ -55,110 +58,12 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-const validateListing = (req,res,next) =>{
-let {error} = listingSchema.validate(req.body);
- if(error){
-  let errMsg =error.details.map((el) => el.message).join(",");
-  throw new ExpressError(400,errMsg);
- }else{
-  next();
- }
-};
-
-const validateReview = (req,res,next) =>{
-let {error} = reviewSchema.validate(req.body);
- if(error){
-  let errMsg =error.details.map((el) => el.message).join(",");
-  throw new ExpressError(400,errMsg);
- }else{
-  next();
- }
-};
-
-//index route
-app.get("/listings", wrapAsync(async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-}));
-
-// New Route
-app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
-
-// Show Route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id).populate("reviews");
-
-  if (!listing) {
-    throw new ExpressError(404, "Listing Not Found");
-  }
-
-  res.render("listings/show.ejs", { listing });
-}));
-
-// Create Route
-app.post("/listings",validateListing, wrapAsync(async (req, res) => {
- 
-  const newListing = new Listing(req.body.listing);
-  
-  await newListing.save();
-  res.redirect("/listings");
-}));
-
-// Edit Route
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
-
-  if (!listing) {
-    throw new ExpressError(404, "Listing Not Found");
-  }
-
-  res.render("listings/edit.ejs", { listing });
-}));
-
-// Update Route
-app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
-  
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-}));
-
-// Delete Route
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-  const { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect("/listings");
-}));
-//Reviews
-//post review route
-app.post("/listings/:id/reviews",validateReview, wrapAsync(async(req, res) => {
-let listing = await Listing.findById(req.params.id);
-let newReview = new Review(req.body.review);
 
 
-listing.reviews.push(newReview);
 
-await newReview.save();
-await listing.save();
+app.use("/listings",listings);
 
-
-res.redirect(`/listings/${listing._id}`);
-
-}));
-
-//DELETE review ROUTE
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req, res) =>{
-let{id,reviewId} = req.params;
-
-await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-await Review.findByIdAndDelete(reviewId);
-res.redirect(`/listings/${id}`);
-})
-);
+app.use("/listings/:id/reviews",reviews);
 
 // ======================
 // 404 Handler (Express 5 compatible)
---
+++ classroom/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+const users = require("./routes/user.js");
+const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+
+
+app.use(cookieParser());
+
+app.get("/getsignedcookie", (req,res) =>{
+    res.cookie("made-in","India",{signed: true});
+    res.send("signed cookie sent");
+});
+app.get("verify",(req,res) => {
+    console.log(req.signedCookies);
+    res.send("verified");
+});
+
+app.get("/", (req, res) =>{
+    console.dir(req.cookies);
+    res.send("hi");
+});
+app.use("/users", users);
+app.use("/posts", posts);
+
+app.listen(3000, () => {
+    console.log("server is listening on port 3000");
+});
